68d9a5753e7cf5fcd6650f437274c2b3503fd2e5
kyle <kcollie@hotmail.com>
2015-06-16 23:57:33 -0400

    working service

---
+++ OrderService/IService1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace OrderService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+           RequestFormat = WebMessageFormat.Json,
+           ResponseFormat = WebMessageFormat.Json,
+           UriTemplate = "/GetOrderMaster/")]
+        List<OrderDataContract.OrderMasterDataContract> GetOrderMaster();
+
+        [OperationContract]
+        [WebGet(RequestFormat = WebMessageFormat.Json,
+           ResponseFormat = WebMessageFormat.Json,
+           UriTemplate = "/SearchOrderMaster/{Order_No}")]
+        OrderDataContract.OrderMasterDataContract SearchOrderMaster(string Order_No);
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+           RequestFormat = WebMessageFormat.Json,
+           ResponseFormat = WebMessageFormat.Json,
+           UriTemplate = "/OrderDetails/{Order_No}")]
+        List<OrderDataContract.OrderDetailDataContract> OrderDetails(string Order_No);
+    }
+
+
+    public class OrderDataContract
+    {
+        [DataContract]
+        public class OrderMasterDataContract
+        {
+            [DataMember]
+            public string Order_No { get; set; }
+            [DataMember]
+            public string Table_ID { get; set; }
+            [DataMember]
+            public string Description { get; set; }
+            [DataMember]
+            public string Order_DATE { get; set; }
+            [DataMember]
+            public string Waiter_Name { get; set; }
+
+        }
+
+
+        [DataContract]
+        public class OrderDetailDataContract
+        {
+            [DataMember]
+            public string Order_Detail_No { get; set; }
+            [DataMember]
+            public string Order_No { get; set; }
+            [DataMember]
+            public string Item_Name { get; set; }
+            [DataMember]
+            public string Notes { get; set; }
+            [DataMember]
+            public string QTY { get; set; }
+            [DataMember]
+            public string Price { get; set; }
+        }
+
+    }
+}
---
+++ OrderService/Service1.svc.cs
@@ -0,0 +1,88 @@
+﻿using OrderService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace OrderService
+{
+
+    public class Service1 : IService1
+    {
+        OrderManagementEntities OME;
+
+        public Service1()
+        {
+            OME = new OrderManagementEntities();
+        }
+
+        public List<OrderDataContract.OrderMasterDataContract> GetOrderMaster()
+        {
+            var query = (from a in OME.OrderMasters select a).Distinct();
+            List<OrderDataContract.OrderMasterDataContract> orderMasterList = new List<OrderDataContract.OrderMasterDataContract>();
+            query.ToList().ForEach(rec =>
+            {
+                orderMasterList.Add(new OrderDataContract.OrderMasterDataContract
+                {
+                    Order_No = Convert.ToString(rec.Order_No),
+                    Table_ID = rec.Table_ID,
+                    Description = rec.Description,
+                    Order_DATE = Convert.ToString(rec.Order_DATE),
+                    Waiter_Name = rec.Waiter_Name
+                });
+            });
+            return orderMasterList;
+        }
+
+        public OrderDataContract.OrderMasterDataContract SearchOrderMaster(string Order_No)
+        {
+            OrderDataContract.OrderMasterDataContract OrderMaster = new OrderDataContract.OrderMasterDataContract();
+
+            try
+            {
+
+                var query = (from a in OME.OrderMasters
+                             where a.Order_No.Equals(Order_No)
+                             select a).Distinct().FirstOrDefault();
+
+                OrderMaster.Order_No = Convert.ToString(query.Order_No);
+                OrderMaster.Table_ID = query.Table_ID;
+                OrderMaster.Description = query.Description;
+                OrderMaster.Order_DATE = Convert.ToString(query.Order_DATE);
+                OrderMaster.Waiter_Name = query.Waiter_Name;
+            }
+            catch (Exception ex)
+            {
+                throw new FaultException<string>
+                       (ex.Message);
+            }
+            return OrderMaster;
+        }
+
+        public List<OrderDataContract.OrderDetailDataContract> OrderDetails(string Order_No)
+        {
+            var query = (from a in OME.OrderDetails
+                         where a.Order_No.Equals(Order_No)
+                         select a).Distinct();
+
+            List<OrderDataContract.OrderDetailDataContract> OrderDetailList = new List<OrderDataContract.OrderDetailDataContract>();
+
+            query.ToList().ForEach(rec =>
+            {
+                OrderDetailList.Add(new OrderDataContract.OrderDetailDataContract
+                {
+                    Order_Detail_No = Convert.ToString(rec.Order_Detail_No),
+                    Order_No = Convert.ToString(rec.Order_No),
+                    Item_Name = rec.Item_Name,
+                    Notes = rec.Notes,
+                    QTY = Convert.ToString(rec.QTY),
+                    Price = Convert.ToString(rec.Price)
+                });
+            });
+            return OrderDetailList;
+        }
+    }
+}
