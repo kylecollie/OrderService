96e3ed2971023fa63b7a739ea135bc71294caad8
kyle <kcollie@hotmail.com>
2015-06-18 18:05:54 -0400

    added db files to project

---
+++ OrderApp/Scripts/KCAngular/Services.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="../angular.js" />
+/// <reference path="../angular.min.js" />
+/// <reference path="Modules.js" />
+
+app.service("AngularJs_WCFService", function ($http) {
+    //Get order master records
+    this.getOrderMaster = function () {
+        return $http.get("http://http://localhost:62871/Service1.svc/GetOrderMaster");
+    };
+
+    //Search order master rcords
+    this.getSearchOrder = function (OrderNO) {
+        return $http.get("http://http://localhost:62871/Service1.svc/SearchOrderMaster/" + OrderNO);
+    }
+
+    //Search order details records
+    this.getOrderDetail = function (OrderNO){
+        return $http.get("http://http://localhost:62871/Service1.svc/OrderDetails/" + OrderNO);
+    }
+});
